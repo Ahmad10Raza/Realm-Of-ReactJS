6b77a45036e9fecceda69a70de9f1463f8d9bad6
Ahmad10Raza <rjaahmad60@gmail.com>
2025-08-02 22:04:37 +0530

    Added PasswordGenerator

--- HelloWord/src/App.jsx
+++ HelloWord/src/App.jsx
@@ -1,48 +1,37 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 
-// First component
-function Welcome() {
-  return <h1>Welcome to React!</h1>;
-}
-
-// Second component
-function Message() {
-  return <p>This is your first multi-component file.</p>;
-}
 
 
 // Main App component rendering both
 function App() {
 
-  let [counter, SetCounter] = useState(15)
-
-const AddValue = () => {
-  if (counter < 19){
-    
-  SetCounter(counter + 1)
-    
-  }
- }
-
- const SubValue = () => {
-  if (counter > 0 ){
-    SetCounter(counter - 1)
-  }
   
- }
+ 
 
   return (
     <div>
-      <Welcome />
-      <Message />
 
+    <div className="relative h-[400px] w-[300px] rounded-md ">
+  <img
+    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    alt="AirMax Pro"
+    className="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-left">
+    <h1 className="text-lg font-semibold text-white">{}</h1>
+    <p className="mt-2 text-sm text-gray-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      debitis?
+    </p>
+    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+      { } →
+    </button>
+  </div>
+</div>
 
-
-    <button onClick={AddValue}>Increment {counter}</button>
-
-    <button onClick={SubValue}>Value {counter}</button>
-    <p>{counter}</p>
+    
 
     </div>
   );
